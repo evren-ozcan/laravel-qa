d72386870cfc0c93ebbe92472f999074f4665cc8
Evren Özcan <evren.ozcan@gmail.com>
2020-01-13 15:50:20 +0300

    additional

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question:class);
     }
+
+    public function setTitleAttribute ($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug']= Str::slug($value);
+    }
 }
