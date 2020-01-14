8ac48d5a4d7c522b96a727de9ff029b2911692f8
Evren Özcan <evren.ozcan@gmail.com>
2020-01-14 12:35:57 +0300

    add author info and question creation date in question item

--- app/Question.php
+++ app/Question.php
@@ -14,9 +14,21 @@ class Question extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
     public function setTitleAttribute ($value) 
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug']= Str::slug($value);
     }
+
+
 }
