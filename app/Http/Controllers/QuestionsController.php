5957bc6a489bbfdedab0cc50c56b71e455df2898
Evren Özcan <evren.ozcan@gmail.com>
2020-01-16 20:08:29 +0300

    Implement delete functionality for the question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -56,7 +56,11 @@ class QuestionsController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        $question->increment('views');
+        //$question->views = $question->views + 1;
+        //$question->save();
+
+        return view('questions.show', compact('question'));
     }
 
     /**
